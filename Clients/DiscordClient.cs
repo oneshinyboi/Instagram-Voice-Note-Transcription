93e297695266927ce825af2559f8841c7cea4ba0
diamond <diamond.white@dart.ky>
2024-08-12 18:24:04 -0400

    discord client started

---
+++ Clients/DiscordClient.cs
@@ -0,0 +1,44 @@
+using Discord;
+using Discord.WebSocket;
+namespace VoiceNoteTranscription.Clients;
+
+public class DiscordClient : Client
+{
+    private static DiscordSocketClient _client;	
+    public DiscordClient(string filepath) : base(filepath)
+    {
+    }
+
+    public override async Task Start()
+    {
+        var config = new DiscordSocketConfig {GatewayIntents = GatewayIntents.MessageContent};
+        _client = new DiscordSocketClient();
+        await _client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("DiscordToken"));
+        await _client.StartAsync();
+
+        _client.MessageReceived += ProcessVoiceNote;
+        
+        _client.Ready += () => 
+        {
+            Console.WriteLine("Bot is connected!");
+            return Task.CompletedTask;
+        };
+        
+        await Task.Delay(-1);
+    }
+
+    private static async Task ProcessVoiceNote(SocketMessage socketMessage)
+    {
+        if (socketMessage.Attachments.Count >= 0)
+        {
+            foreach (var attachment in socketMessage.Attachments)
+            {
+                Console.WriteLine(attachment.ContentType);
+                if (attachment.ContentType == "audio")
+                {
+                    Console.WriteLine(attachment.Url);
+                }
+            }
+        }
+    }
+}
